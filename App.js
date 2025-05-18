92cf3fcc275f89742464e439d8c2c73bb1604887
ShaquilleNathan <shaquillenathankalevi@gmail.com>
2025-05-18 15:32:28 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './src/screens/HomeScreen';
+import GuessTheCountryScreen from './src/screens/GuessTheCountryScreen';
+import WinScreen from './src/screens/WinScreen';
+import MainNavigator from './src/navigator/MainNavigator';
+
+const App = () => {
+  return (
+   <SafeAreaProvider>
+     {/* <HomeScreen/>
+     <GuessTheCountryScreen/>
+     <WinScreen/> */}
+     <MainNavigator/>
+   </SafeAreaProvider>
+  )
+};
+
+export default App;
